8871e560542f195eaff3652b2c1fb5ef05abc54f
LouisEvans <35015723+LouisEvans@users.noreply.github.com>
2019-02-25 18:25:19 +0000

    Initial commit

---
+++ src/hotel/HotelImpl.java
@@ -0,0 +1,15 @@
+package hotel;
+
+//abstract class HotelImpl implements hotel.Hotel {
+public class HotelImpl{
+
+    public static void main(String args[]){
+        System.out.println("Hello World!");
+
+        System.out.println(importRoomsData("RoomType"));
+    }
+
+    public static boolean importRoomsData(String RoomType){
+        return true;
+    }
+}
